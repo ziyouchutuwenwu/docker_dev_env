38b18f79157f72c217304f36cf38374beaac514a
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2023-10-27 15:43:59 +0800

    .

--- fix_permission.sh
+++ fix_permission.sh
@@ -2,6 +2,7 @@
 
 sudo chown -R `whoami` ~/projects/docker/
 sudo chgrp -R `whoami` ~/projects/docker/
+sudo chmod -R g=rwx    ~/projects/docker/
 
 sudo chown -R 65534 ~/projects/docker/nfs
 sudo chgrp -R 65534 ~/projects/docker/nfs
