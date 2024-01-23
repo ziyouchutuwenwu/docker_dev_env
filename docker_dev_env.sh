96aad13575f20178cd8cbf91e114b3211ad078b4
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2024-01-23 13:41:34 +0800

    .

--- docker_dev_env.sh
+++ docker_dev_env.sh
@@ -26,7 +26,7 @@ docker run --rm -d --name ftp -v ~/projects/docker/ftp/:/home/vsftpd/demo -p 20:
 docker run --rm -d -it --name=tftpd -p 69:69/udp -v ~/projects/docker/tftp:/srv/tftp hkarhani/tftpd
 
 # 共享目录为 \\ip\smb
-docker run --rm -d --name smb -v ~/projects/docker/smb/:/mount -p 139:139 -p 445:445 dperson/samba -u "mmc;123456;1000;mmc;1000" -s "smb;/mount;yes;no;no;mmc" -p
+docker run --rm -d --name smb -v ~/projects/docker/smb/:/mount -p 139:139 -p 445:445 dperson/samba -u "mmc;123456;1000;mmc;1000" -s "smb;/mount;yes;no;no;mmc"
 
 # metasploit
 # alias msf="docker exec -it msf /usr/src/metasploit-framework/msfconsole"
